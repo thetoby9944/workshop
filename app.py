7cad2a51e12a03bd44745d50a6ba2010965b092e
Tobias Schiele <thetoby@web.de>
2022-02-14 15:14:41 +0100

    Assignment 1

--- app.py
+++ app.py
@@ -4,3 +4,54 @@ st.write("Hello workshop")
 
 
 st.selectbox("Select one option", ["A", "B"])
+
+
+
+
+import os
+import os.path
+import pandas as pd
+import tempfile
+import urllib.request
+
+WD = os.path.join(tempfile.gettempdir(), "brainvol")
+os.makedirs(WD, exist_ok=True)
+#os.chdir(WD)
+
+# use cookiecutter file organization
+# https://drivendata.github.io/cookiecutter-data-science/
+os.makedirs(os.path.join(WD, "data"), exist_ok=True)
+#os.makedirs("reports", exist_ok=True)
+
+
+
+base_url = 'https://github.com/duchesnay/pystatsml/raw/master/datasets/brain_volumes/%s'
+data = dict()
+for file in ["demo.csv", "gm.csv", "wm.csv", "csf.csv"]:
+    urllib.request.urlretrieve(base_url % file, os.path.join(WD, "data", file))
+
+# Read all CSV in one line
+# dicts = {k: pd.read_csv(os.path.join(WD, "data", "%s.csv" % k))
+#          for k in ["demo", "gm", "wm", "csf"]}
+
+demo = pd.read_csv(os.path.join(WD, "data", "demo.csv"))
+gm = pd.read_csv(os.path.join(WD, "data", "gm.csv"))
+wm = pd.read_csv(os.path.join(WD, "data", "wm.csv"))
+csf = pd.read_csv(os.path.join(WD, "data", "csf.csv"))
+
+st.write("tables can be merge using shared columns")
+st.write(gm.head())
+
+
+brain_vol = pd.merge(pd.merge(pd.merge(demo, gm), wm), csf)
+assert brain_vol.shape == (808, 9)
+
+
+brain_vol = brain_vol.dropna()
+assert brain_vol.shape == (766, 9)
+
+
+if st.button("Calcualte Total intra cranical volume"):
+    brain_vol["tiv_vol"] = brain_vol["gm_vol"] + brain_vol["wm_vol"] + brain_vol["csf_vol"]
+
+    st.write(brain_vol["tiv_vol"])
