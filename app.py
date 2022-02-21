6f530f637b10a5364c73501fb75adb3f25d99385
Tobias Schiele <thetoby@web.de>
2022-02-21 15:39:14 +0100

    workshop - pathlib

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
+from pathlib import Path
+
 import streamlit as st
+import numpy as np
+import PIL
+from PIL import Image
 import plotly.express as px
+from pydantic import BaseModel
 
 
-st.write("Hello workshop")
+"# Image Processing Workshop"
+
+st.image("image.jpg")
+
+path = Path("image.jpg")
+path
+
+path = path.resolve()
+path
+
+path = path.parent
+path
+
+path = path / "image.jpg"
+path
+
+path_list = list(path.parent.rglob("*.jpg"))
+path_list
+
+
+
 
